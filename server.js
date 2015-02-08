94c823293fdc87faff1204b6d3baef676ceb6715
cstumph <cjstumph@gmail.com>
2015-02-08 02:23:52 -0800

    update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('Hello, world!');
+        reply('Hello human.');
     }
 });
 server.route({
